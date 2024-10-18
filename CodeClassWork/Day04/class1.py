7cb95166d7f5166f6d92f15b806636ef955fbf74
bhimatak <bhima.t@gmail.com>
2024-10-18 11:36:28 +0530

    updated Day04

--- CodeClassWork/Day04/class1.py
+++ CodeClassWork/Day04/class1.py
@@ -9,7 +9,7 @@ class Person:
     # Age = 45
     # Gender = "M"
 
-    def __init__(self,name,phno,g):
+    def __init__(self,name="",phno=0,g=""):
         #init variables
         print("Constructor Invoked")
         self._fName = name
@@ -38,8 +38,25 @@ class Person:
     def getAge(self):
         return self._age
 
-    def setProperties(self):
-        
+    def setProperties(self, name,phno,age,g):
+        print("\nSetting the Properties of Person")
+        self._fName = name
+        self._age =age
+        self._phno = phno
+        self._gender =g
+
+    def setPropUI(self):
+        print("\nEnter Value of Person")
+        self._fName = input("Name: ")
+        self._phno = int(input("Phno: "))
+        self._age = int(input("Age: "))
+        self._gender = input("Gender: ")
+
+    def displayPerson(self):
+        print("\nPerson Details are\n")
+        print("\n{}\n{}\n{}\n{}".format(self._fName,self._phno,self._age,self._gender))
+
+
 
 
 '''        
@@ -72,3 +89,11 @@ print(p2.getName(), p2.getPhno(), p2.getGender())
 p3 = Person("Bhima",999,"M")
 print(p3.getName(), p3.getPhno(), p3.getGender(), p3.getAge())
 
+p3.displayPerson()
+
+p4 = Person()
+p4.setProperties("Shankar",888,45,g="M")
+p4.displayPerson()
+p5 = Person()
+p5.setPropUI()
+p5.displayPerson()
