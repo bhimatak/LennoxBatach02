817a3d1d0ab71ff1c268aadd07a23fb6dc925373
bhimatak <bhima.t@gmail.com>
2024-10-18 16:03:46 +0530

    updated Day04

--- CodeClassWork/Day04/assignment1.py
+++ CodeClassWork/Day04/assignment1.py
@@ -1,3 +1,4 @@
+'''
 OOP Exercise : Class Inheritance
 Given:
 
@@ -6,3 +7,4 @@ Create a Bus child class that inherits from the Vehicle class. The default fare 
 Note: The bus seating capacity is 50. so the final fare amount should be 5500. You need to override the fare() method of a Vehicle class in Bus class.
 
 Use the following code for your parent Vehicle class. We need to access the parent class from inside a method of a child class.
+'''
